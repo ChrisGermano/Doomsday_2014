7636350fd60ed0bbff90b5f7f07df2cedc703dcb
duncan-macleod <troxnor@gmail.com>
2014-01-25 09:58:22 -0500

    ai changes

---
+++ GGJ2014/Assets/AIPath.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class AIPath : MonoBehaviour {
+
+	public float Speed = 0.1f;
+	
+	System.Random r;
+
+	// Use this for initialization
+	void Start () {
+		r = new System.Random();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Translate (transform.forward * Speed);
+
+	}
+
+	void OnTriggerEnter(Collider c){
+		double y = r.NextDouble() * 360;
+		transform.Rotate(new Vector3(0, (float)y, 0));
+	}
+}
