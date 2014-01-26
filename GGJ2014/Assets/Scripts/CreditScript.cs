6aab7a3414c283dcc27a811ea96930491c0e004b
duncan-macleod <troxnor@gmail.com>
2014-01-26 11:08:45 -0500

    Credits screen fixed

--- GGJ2014/Assets/Scripts/CreditScript.cs
+++ GGJ2014/Assets/Scripts/CreditScript.cs
@@ -7,6 +7,30 @@ public class CreditScript : MonoBehaviour {
 
 	void Start () {
 		endTime = Time.time + 10;
+		UnityEngine.Resolution res = Screen.currentResolution;
+		int height = res.height;
+		int width = res.width;
+		GameObject credits = GameObject.Find("Credits");
+		TextMesh[] names = credits.GetComponentsInChildren<TextMesh>();
+		float c_width = 0;
+		float c_height = 0;
+
+		foreach(TextMesh name in names){
+			float w = name.renderer.bounds.size.x;
+			float h = name.renderer.bounds.size.z;
+			if(w > c_width){
+				c_width = w;
+			}
+
+			c_height += h;
+			c_height += 10;
+		}
+		c_height -= 10;
+
+
+		credits.transform.position = new Vector3(0 - c_width/2, 
+		                                         credits.transform.position.y, 
+		                                         0);
 	}
 	
 	void Update () {
