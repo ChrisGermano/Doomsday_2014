8cb7098f4b53314ceb3c06268aad2e8967583c93
duncan-macleod <troxnor@gmail.com>
2014-01-26 11:49:24 -0500

    Fade in and out

--- GGJ2014/Assets/Scripts/EndGame.cs
+++ GGJ2014/Assets/Scripts/EndGame.cs
@@ -24,6 +24,8 @@ public class EndGame : MonoBehaviour {
 	private float dist;
 	private float speed = 40f;
 
+	bool LevelEnding = false;
+
 	// Use this for initialization
 	void Start () {
 		RenderSettings.skybox.SetColor("_Tint", new Color(0f, 0f, 1f));
@@ -35,42 +37,64 @@ public class EndGame : MonoBehaviour {
 	}
 
 	public void EndUpdate () {
-		float movedDist = (Time.time - startT) * speed;
-		float perDist = movedDist / dist;
-		meteor.transform.position = Vector3.Lerp(meteorStart, new Vector3(0f,0f,0f), perDist);
-		if (!doomsday) {
-			meteor.transform.localScale = Vector3.Lerp (meteorScale, new Vector3(0f,0f,0f), perDist);
-			meteor.GetComponent<ParticleSystem>().startLifetime = Mathf.Lerp (meteorFlameScale, 0f, perDist);
-			RenderSettings.skybox.SetColor("_Tint", Color.Lerp(skyboxC, goodColor, perDist));
-			if (mainC.GetComponent<AudioSource>().isPlaying) {
-				mainC.GetComponent<AudioSource>().volume = Mathf.Lerp (0f, 1f, perDist);
+		if(!LevelEnding){
+			float movedDist = (Time.time - startT) * speed;
+			float perDist = movedDist / dist;
+			meteor.transform.position = Vector3.Lerp(meteorStart, new Vector3(0f,0f,0f), perDist);
+			if (!doomsday) {
+				meteor.transform.localScale = Vector3.Lerp (meteorScale, new Vector3(0f,0f,0f), perDist);
+				meteor.GetComponent<ParticleSystem>().startLifetime = Mathf.Lerp (meteorFlameScale, 0f, perDist);
+				RenderSettings.skybox.SetColor("_Tint", Color.Lerp(skyboxC, goodColor, perDist));
+				if (mainC.GetComponent<AudioSource>().isPlaying) {
+					mainC.GetComponent<AudioSource>().volume = Mathf.Lerp (0f, 1f, perDist);
+				} else {
+					mainC.GetComponent<AudioSource>().clip = saved;
+					mainC.GetComponent<AudioSource>().Play();
+				}
 			} else {
-				mainC.GetComponent<AudioSource>().clip = saved;
-				mainC.GetComponent<AudioSource>().Play();
+				mainC.GetComponent<NoiseEffect>().grainIntensityMin = Mathf.Lerp (0f, 0.5f, perDist);
+				mainC.GetComponent<NoiseEffect>().grainIntensityMax = Mathf.Lerp (0f, 3f, perDist);
+				mainC.GetComponent<NoiseEffect>().grainSize = Mathf.Lerp (0f, 5f, perDist);
+				if (mainC.GetComponent<AudioSource>().isPlaying) {
+					mainC.GetComponent<AudioSource>().volume = Mathf.Lerp (0f, 0.35f, perDist);
+				} else {
+					mainC.GetComponent<AudioSource>().clip = doom;
+					mainC.GetComponent<AudioSource>().Play();
+				}
+				RenderSettings.skybox.SetColor("_Tint", Color.Lerp(skyboxC, badColor, perDist));
 			}
-		} else {
-			mainC.GetComponent<NoiseEffect>().grainIntensityMin = Mathf.Lerp (0f, 0.5f, perDist);
-			mainC.GetComponent<NoiseEffect>().grainIntensityMax = Mathf.Lerp (0f, 3f, perDist);
-			mainC.GetComponent<NoiseEffect>().grainSize = Mathf.Lerp (0f, 5f, perDist);
-			if (mainC.GetComponent<AudioSource>().isPlaying) {
-				mainC.GetComponent<AudioSource>().volume = Mathf.Lerp (0f, 0.35f, perDist);
-			} else {
-				mainC.GetComponent<AudioSource>().clip = doom;
-				mainC.GetComponent<AudioSource>().Play();
+
+			if (perDist >= 1) {
+				this.StartCoroutine(EndLevel());
+				LevelEnding = true;
+			} else if (perDist > 0.8f) {
+
+				if (!RenderSettings.fog) {
+					RenderSettings.fog = true;
+				}
+
+				RenderSettings.fogDensity = (0.2f - (1 - perDist))/2;
 			}
-			RenderSettings.skybox.SetColor("_Tint", Color.Lerp(skyboxC, badColor, perDist));
 		}
+		
+	}
 
-		if (perDist >= 1) {
-			Application.LoadLevel("CreditScene");
-		} else if (perDist > 0.8f) {
-
-			if (!RenderSettings.fog) {
-				RenderSettings.fog = true;
+	private IEnumerator EndLevel()
+	{
+		LoadLevel lvl = GetComponent<LoadLevel>();
+		bool waiting = true;
+		lvl.FadeOut();
+		while(waiting)
+		{
+			if(lvl.IsFading())
+			{
+				yield return 0;
 			}
-
-			RenderSettings.fogDensity = (0.2f - (1 - perDist))/2;
+			else 
+			{
+				waiting = false;
+				Application.LoadLevel("CreditScene");
+			}
 		}
-
 	}
 }
